422d877f5e2f860d9743d3d84bfd26706d31fb9f
SultanFazalika <72803256+SultanFazalika@users.noreply.github.com>
2020-11-19 11:16:47 +0700

    Add files via upload

---
+++ praktikum4.py
@@ -0,0 +1,46 @@
+
+a = [30, 40, 50]
+b = [10, 20]
+
+#menambah data
+#b.append(10)
+
+#menggabungkan list
+c = a + b
+a.extend(b)
+
+#menmbah 3 data
+#b.extend([60, 90, 70])
+
+#mengubah data
+#a[3] = 200
+#a[3:4] = [200, 300]
+
+#elemen ke 3
+#print(a[2])
+
+#elemen ke 2-4
+#print(a[1:4])
+
+#elemen ke 5
+#print(a[4])
+
+#tampilan  print b.append
+#print(b[0:3])
+
+#tampilan print b.extend
+#print(b[0:5])
+
+print (c)
+
+
+
+
+
+
+
+
+
+
+
+
